330e7c7e1bdae81cbe5d7b12d9239587ca054e33
Filip Toloczko <ftolo2@uic.edu>
2024-04-26 23:52:26 -0500

    graph full of entries

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<PieChartSectionData> showingSections(){
     return List.generate(
-      5,
+      6,
       (i){
         switch(i){
           case 0:
@@ -80,35 +80,42 @@ class _MyHomePageState extends State<MyHomePage> {
               color: Colors.grey,
               value: 3,
               title: 'Sox',
-              radius: 100
+              radius: 150
             );
           case 1:
             return PieChartSectionData(
                 color: Colors.blue.shade800,
                 value: 3,
                 title: 'Cubs',
-                radius: 100
+                radius: 150
             );
           case 2:
             return PieChartSectionData(
                 color: Colors.orange,
                 value: 9,
                 title: 'Bears',
-                radius: 100
+                radius: 150
             );
           case 3:
             return PieChartSectionData(
                 color: Colors.redAccent.shade700,
                 value: 6,
                 title: 'Bulls',
-                radius: 100
+                radius: 150
             );
           case 4:
+            return PieChartSectionData(
+                color: Colors.green,
+                value: 6,
+                title: 'Blackhawks',
+                radius: 150
+            );
+          case 5:
             return PieChartSectionData(
                 color: Colors.red.shade400,
                 value: 5,
                 title: 'Fire',
-                radius: 100
+                radius: 150
             );
           default:
             throw Error();
